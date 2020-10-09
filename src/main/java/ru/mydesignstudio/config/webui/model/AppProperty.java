6e6d3879d24642ba913f772cd8bb1619aec45fb2
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-10-09 16:00:48 +0300

    #1 Adding model classes

---
+++ src/main/java/ru/mydesignstudio/config/webui/model/AppProperty.java
@@ -0,0 +1,53 @@
+package ru.mydesignstudio.config.webui.model;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Property that is used to configure the application.
+ */
+@Data
+@Document("config.property")
+public class AppProperty {
+    @Id
+    private ObjectId propertyId;
+
+    /**
+     * Property key that is used in the application. It's assumed that this value is unique across the whole
+     * configuration or at least has the same meaning for different services.
+     */
+    private String key;
+    /**
+     * Human-readable name of the property.
+     */
+    private String name;
+    /**
+     * Human-readable description of the property.
+     */
+    private String description;
+    /**
+     * A small example that describes what values this property can has.
+     */
+    private String example;
+    /**
+     * A regular expression that is used for validation of the value for this property.
+     */
+    private String validationExpression;
+    /**
+     * This flag shows that this is a common property and it's shared across all the services.
+     */
+    private boolean commonProperty;
+    /**
+     * A collection that stores keys of the services that use this property.
+     */
+    private Set<String> services = new HashSet<>();
+    /**
+     * What data type should be used to store the value.
+     */
+    private PropertyType propertyType;
+}
---
+++ src/main/java/ru/mydesignstudio/config/webui/model/PropertyType.java
@@ -0,0 +1,9 @@
+package ru.mydesignstudio.config.webui.model;
+
+/**
+ * Property types.
+ */
+public enum PropertyType {
+    STRING,
+    BOOLEAN
+}
