6af4574375a6afb5b773118b31a9b62eecbe8c2f
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-10-18 12:32:38 +0300

    #30 Extracting a separate component to add properties

--- webapp/src/main/resources/static/js/components/config_common.vue
+++ webapp/src/main/resources/static/js/components/config_common.vue
@@ -1,22 +1,16 @@
 <template>
   <div>
-    <div class="form-group row">
-      <label class="col-sm-2 col-form-label">Available properties:</label>
-      <select class="form-control col-sm-8" v-model="selectedType">
-        <option v-for="property in availableProperties"
-                v-bind:value="property.key"
-                :key="property.key">
-          {{property.key}} - {{property.description}}
-        </option>
-      </select>
-      <div class="col-sm-2">
-        <button type="button" class="btn btn-primary" v-on:click="addProperty">
-          Add
-        </button>
+    <div class="row">
+      <div class="col-sm-12">
+        <property-appender v-bind:all-properties="availableProperties"
+                           v-bind:used-properties="values" />
       </div>
     </div>
-
-    <property-list v-bind:properties="values" />
+    <div class="row">
+      <div class="col-sm-12">
+        <property-list v-bind:properties="values" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,45 +19,19 @@ module.exports = {
   props: ['values', 'properties'],
   data: function(){
     return {
-      selectedProperty: ''
+
     }
   },
   computed: {
     availableProperties: function() {
       return this.properties.filter(prop => {
-        let used = this.values.filter(selected => {
-          return selected.property.key == prop.key;
-        });
-        return (used.length == 0) && (prop.isCommon == true);
-      });
-    },
-    selectedType: {
-      get: function() {
-        if (this.availableProperties.length > 0) {
-          this.selectedProperty = this.availableProperties[0].key;
-          return this.selectedProperty;
-        }
-        return '';
-      },
-      set: function(newValue) {
-        this.selectedProperty = newValue;
-      }
-    }
-  },
-  methods: {
-    addProperty: function() {
-      let property = this.properties.filter(prop => {
-        return prop.key == this.selectedProperty;
-      })[0];
-
-      this.values.push({
-        property: property,
-        value: ''
+        return prop.isCommon === true;
       });
     }
   },
   components: {
-    'property-list': httpVueLoader("/js/components/property_list.vue")
+    'property-list': httpVueLoader("/js/components/property_list.vue"),
+    'property-appender': httpVueLoader("/js/components/property_appender.vue")
   }
 }
 </script>
